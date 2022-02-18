67a08cb7c1d07a7151425b8a6c66ffd0de1a1e72
kylee <leeky940926@gmail.com>
2022-02-18 21:35:34 +0900

    aggregate&annotate

--- querysets/urls.py
+++ querysets/urls.py
@@ -1,6 +1,8 @@
 from django.urls     import path
-from querysets.views import get_all, filter, exclude, aggregate, alias, annotate, order_by, distinct, select_related, prefetch_related, select_for_update, BeforePrefetchView
-
+from querysets.views import (
+    get_all, filter, exclude, aggregate, alias, annotate, order_by, distinct, select_related, prefetch_related, select_for_update, 
+    BeforePrefetchView
+)
 urlpatterns = [
     path('/all', get_all),
     path('/filter', filter),
@@ -13,5 +15,5 @@ urlpatterns = [
     path('/select_related', select_related),
     path('/prefetch_related', prefetch_related),
     path('/select_for_update', select_for_update),
-    path('/before-prefetch/<int:city_id>', BeforePrefetchView.as_view())
+    path('/before-prefetch/<int:city_id>', BeforePrefetchView.as_view()),
 ]
